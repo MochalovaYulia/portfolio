7d18add5e915ebae55fdbfbbf5062c49f767aaad
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-02-27 23:02:37 +0300

    add component header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { CTA } from './CTA'
 import './header.css'
 
 export const Header = () => {
   return (
-    <div></div>
+    <header>
+      <div className='container header__container'>
+        <h5>Привет, Я</h5>
+        <h1>Юлия Мочалова</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+
+        <div className='me'>
+        <img src='' alt='me' />
+      </div>
+
+        <a href='#contact' className='scroll__down'>Прокрутить вниз</a>
+      </div>
+    </header>
   )
 }
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://github.com' target='_blank'></a>
+    </div>
+  )
+}
