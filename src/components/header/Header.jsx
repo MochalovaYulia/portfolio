20385415354939029c00cecd7dd91e37006c0232
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-02-28 21:38:09 +0300

    header component + css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { CTA } from './CTA'
 import './header.css'
+import me from '../../assets/me.png'
+import { HeaderSocials } from './HeaderSocials'
 
 export const Header = () => {
   return (
@@ -10,9 +12,10 @@ export const Header = () => {
         <h1>Юлия Мочалова</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className='me'>
-        <img src='' alt='me' />
+        <img src={me} alt='me' />
       </div>
 
         <a href='#contact' className='scroll__down'>Прокрутить вниз</a>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {BsGithub} from 'react-icons/bs'
+import {SlSocialVkontakte} from 'react-icons/sl'
+import {BsInstagram} from 'react-icons/bs'
 
 export const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://github.com' target='_blank'></a>
+        <a href='https://github.com' target='_blank'><BsGithub /></a>
+        <a href='https://vk.com/julsmoo' target='_blank'><SlSocialVkontakte /></a>
+        <a href='https://www.instagram.com/julliamochalova' target='_blank'><BsInstagram /></a>
     </div>
   )
 }
