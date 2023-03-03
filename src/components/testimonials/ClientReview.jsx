26aa806dc4331dd3eea4e0388b85175240ef6b03
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-03-03 22:42:06 +0300

    testimonials component jsx + css

---
+++ src/components/testimonials/ClientReview.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+    {
+        avatar: AVTR1,
+        name: 'Tina Snow',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur modi nam alias quibusdam mollitia architecto similique voluptates animi. Hic nisi esse minus quis nihil earum soluta necessitatibus accusamus repellat'
+    },
+    {
+        avatar: AVTR2,
+        name: 'Shatta Wale',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur modi nam alias quibusdam mollitia architecto similique voluptates animi. Hic nisi esse minus quis nihil earum soluta necessitatibus accusamus repellat'
+    },
+    {
+        avatar: AVTR3,
+        name: 'Kwame Despite',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur modi nam alias quibusdam mollitia architecto similique voluptates animi. Hic nisi esse minus quis nihil earum soluta necessitatibus accusamus repellat'
+    },
+    {
+        avatar: AVTR4,
+        name: 'Nana Ama McBrown',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur modi nam alias quibusdam mollitia architecto similique voluptates animi. Hic nisi esse minus quis nihil earum soluta necessitatibus accusamus repellat'
+    }
+]
+export const ClientReview = () => {
+    return (
+        <>
+            {
+                data.map(({ avatar, name, review }, index) => {
+                    return (
+                        <article key={index} className='testimonial'>
+                            <div className='client__avatar'>
+                                <img src={avatar} alt={name}/>
+                            </div>
+                            <h5 className='client__name'>{name}</h5>
+                            <small className='client__review'>{review}</small>
+                        </article>
+                    )
+                })
+            }
+        </>
+    )
+}
+
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { ClientReview } from './ClientReview'
 import './testimonials.css'
 
 export const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <div className='container testimonial__container'>
+        <ClientReview />
+      </div>
+    </section>
   )
 }
