2ec459a37d9bf547ee55854a3408bd3015143c1c
Yulia <Yulia.Mochalova.2001@mail.ru>
2023-03-04 23:24:55 +0300

    footer component jsx + css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {BsInstagram} from 'react-icons/bs'
+import {FaFacebookF} from 'react-icons/fa'
+import {IoLogoTwitter} from 'react-icons/io'
 
 export const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Logo</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='footer_sosials'>
+        <a href='https://facebook.com'> <FaFacebookF/> </a>
+        <a href='https://instagram.com'> <BsInstagram/> </a>
+        <a href='https://twitter.com'> <IoLogoTwitter/> </a>
+      </div>
+      <div className='footer__copyright'>
+        <small>&copy; All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
